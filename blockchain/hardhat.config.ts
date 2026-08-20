9429b5398ace3dfe39c41643942e425d74618337
Amey <bondreamey2006@gmail.com>
2026-08-20 22:58:47 +0530

    WIP: Pausing tracechain for MIC recruitment project

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -12,11 +12,6 @@ const config: HardhatUserConfig = {
       viaIR: true,
     },
   },
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
 };
 
 export default config;
